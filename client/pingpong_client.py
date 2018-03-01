28107e734e41a222d56329f382ff1cbe288b7a88
Anton Makridin <a.i.makridin@yandex.ru>
2018-03-01 15:59:01 +0300

    python36, volumes io, client/server upd

--- client/pingpong_client.py
+++ client/pingpong_client.py
@@ -8,12 +8,17 @@ import pingpong_pb2_grpc
 import sys
 
 def run():
-	credentials = grpc.ssl_channel_credentials(root_certificates=open('/pubkey/pingpong.crt').read())
+	credentials = grpc.ssl_channel_credentials(root_certificates=open("/pubkey/pingpong.crt","rb").read())
 	channel = grpc.secure_channel('pingpong:50051', credentials)
-	# channel = grpc.insecure_channel('localhost:50051')
 	stub = pingpong_pb2_grpc.CommandHandlerStub(channel)
 	msg = stub.Respond(pingpong_pb2.command(commandText=sys.argv[1]))
-	print("ANSWER: %s" % msg.responseText)
+
+	if(msg.responseText == "FETCH_DB_FILE"):
+		file = open("/db/response","rt",encoding="utf-8")
+		print("ANSWER_FROM_DB: %s" % file.read())
+		file.close()
+	else:
+		print("ANSWER: %s" % msg.responseText)
 
 
 if __name__ == '__main__':
--- server/pingpong_server.py
+++ server/pingpong_server.py
@@ -10,26 +10,38 @@ import pingpong_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+#create db connection
+db = psycopg2.connect("host='db' dbname='postgres' user='pp_user' password='123'")
+query = db.cursor()
+
 class CommandHandler(pingpong_pb2_grpc.CommandHandlerServicer):
     def Respond(self,request,context):
-    	if request.commandText == "PING":
-        	return pingpong_pb2.response(responseText="PONG")
+        if request.commandText == "PING":
+            return pingpong_pb2.response(responseText="PONG")
+        elif request.commandText == "FROMDB":
+            doQuery()
+            return pingpong_pb2.response(responseText="FETCH_DB_FILE")
         else:
-        	return pingpong_pb2.response(responseText="BAD COMMAND")
+            return pingpong_pb2.response(responseText="BAD COMMAND")
 
+def doQuery():
+    #do simple query
+    query.execute("SELECT msg FROM main WHERE id = 1")
+    collection = query.fetchall()
+
+    #store first tuple from first list into a text file
+    file = open("/db/response","wt",encoding="utf-8")
+    file.write(collection[0][0])
+    file.close()
 
 def main():
-
-    #read SSL keys and create server
-    server_credentials = grpc.ssl_server_credentials(((open('SSL/pingpong.key').read(), open('SSL/pingpong.crt').read(),),))
+    #read SSL keys and create gRPC server
+    server_credentials = grpc.ssl_server_credentials(((open("SSL/pingpong.key","rb").read(), open("SSL/pingpong.crt","rb").read(),),))
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pingpong_pb2_grpc.add_CommandHandlerServicer_to_server(CommandHandler(), server)
     server.add_secure_port('[::]:50051',server_credentials)
 
-    #create db connection
-    db = psycopg2.connect("host='db' dbname='postgres' user='pp_user' password='123'")
-
-    #start grpc server
+    #start server
     server.start()
     try:
         while True:
